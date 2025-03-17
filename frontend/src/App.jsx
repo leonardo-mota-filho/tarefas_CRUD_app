e18824cb288bfe59c9ad4b3fb6b83b8f732fb29c
Leonardo Mota Meira Filho <leonardo.mota.meira.filho@ccc.ufcg.edu.br>
2025-03-17 17:41:14 -0300

    Inicialização das routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,34 +2,41 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Routes, Route, Link, BrowserRouter} from "react-router-dom";
+import TaskList from "./pages/TaskList";
+import AddTask from "./pages/AddTask";
+import Task from "./pages/Task";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/*NAVBAR*/}
+          <nav className="bg-blue-600 p-4 text-white">
+            <div className="flex space-x-4">
+              <Link to="/tasks" className="hover:text-gray-300">
+              Tasks
+              </Link>
+              <Link to="/add" className="hover:text-gray-300">
+              Add
+              </Link>
+            </div>
+          </nav>
+
+        {/*ROUTES*/}
+        <div className="container mx-auto mt-8 px-4">
+          <Routes>
+            <Route path='/' element={<TaskList/>}/>
+            <Route path='/tasks' element={<TaskList/>}/>
+            <Route path='/add' element={<AddTask/>}/>
+            <Route path='/tasks/:id' element={<Task/>}/>
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
 export default App
